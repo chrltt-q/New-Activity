8a67a98e430c04182438dca75294f607feac202b
chrltt-q <charlottequezada17@gmail.com>
2022-11-19 18:49:01 +0800

    confirmation

--- main.py
+++ main.py
@@ -5,12 +5,29 @@ print("*** Sir Danilo Madrigalejos ***\n")
 
 import phonenumbers
 import folium
-from MyPhoneNumber import phone_number
 from phonenumbers import geocoder
 
+
+def confirmation():
+    while True:
+        print("This program is solely intended for tracking down someone in case of emergencies.")
+        confirm = input("Would you like to proceed? Type yes or no: ")
+        if confirm == "yes":
+            break
+        elif confirm == "no":
+            print("Now closing the program. Thank you!")
+            break
+        else:
+            print("Invalid answer, please type yes or no only.")
+            continue
+
+print("Welcome!")
+confirmation()
+
+from MyPhoneNumber import phone_number
 person_number = phonenumbers.parse(phone_number)
 person_location = geocoder.description_for_number(person_number, "en")
-print(person_location)
+print("Country:", person_location)
 
 # Get the service provider
 from phonenumbers import carrier
@@ -28,7 +45,7 @@ lat = results[0]["geometry"]["lat"]
 lng = results[0]["geometry"]["lng"]
 print("Latitude: "f"{lat}\nLongitude: {lng}")
 
-person_map = folium.Map(location=[lat, lng], zoom_start=9)
+person_map = folium.Map(location=[lat, lng], zoom_start=3)
 folium.Marker([lat, lng], popup=person_location).add_to(person_map)
 
 # Save the map in a HTML file
