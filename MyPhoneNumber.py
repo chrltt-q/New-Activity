9fbc50a964fa61ae16468a2f46c525c4299a3c7a
chrltt-q <charlottequezada17@gmail.com>
2022-11-19 17:51:29 +0800

    service_provider

--- MyPhoneNumber.py
+++ MyPhoneNumber.py
@@ -1,3 +1,3 @@
-# Get user input of phone number they want to track
+# Get the user input of phone number they want to track
 track_number = input("Please enter the phone number you want to track: +")
 phone_number = str("+"f"{track_number}")
--- main.py
+++ main.py
@@ -4,12 +4,14 @@ print("********** BSCOE 2-2 **********")
 print("*** Sir Danilo Madrigalejos ***\n")
 
 import phonenumbers
-
 from MyPhoneNumber import phone_number
-
 from phonenumbers import geocoder
-
 
 person_number = phonenumbers.parse(phone_number)
 person_location = geocoder.description_for_number(person_number, "en")
 print(person_location)
+
+# Get the service provider
+from phonenumbers import carrier
+service_provider = phonenumbers.parse(phone_number)
+print(carrier.name_for_number(service_provider, "en"))
