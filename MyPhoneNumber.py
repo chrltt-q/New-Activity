1536dc964184d077152a0f5c20ddeecef82e7443
chrltt-q <charlottequezada17@gmail.com>
2022-11-19 18:57:44 +0800

    print

--- MyPhoneNumber.py
+++ MyPhoneNumber.py
@@ -1,5 +1,5 @@
 # Get the user input of phone number they want to track
-track_number = input("Please enter the phone number you want to track: +")
+track_number = input("\nPlease enter the phone number you want to track: +")
 phone_number = str("+"f"{track_number}")
 
 # Get the user input for key by creating an account in the website opencagedata.com
