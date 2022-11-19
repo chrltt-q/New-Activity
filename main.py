9f4b2b8db7a9883d4db6675e7686c89f020ff47e
chrltt-q <charlottequezada17@gmail.com>
2022-11-19 17:35:07 +0800

    import

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ print("******** PROGRAMMED BY ********")
 print("****** Charlotte Quezada ******")
 print("********** BSCOE 2-2 **********")
 print("*** Sir Danilo Madrigalejos ***\n")
+
+import phonenumbers
+
+from MyPhoneNumber import phone_number
+
+from phonenumbers import geocoder
+
+
