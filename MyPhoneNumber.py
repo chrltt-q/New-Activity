e7fb9b1b92ea175cdd30833f74cc38ec6324bfd7
chrltt-q <charlottequezada17@gmail.com>
2022-11-19 17:46:13 +0800

    track_number

--- MyPhoneNumber.py
+++ MyPhoneNumber.py
@@ -1,2 +1,3 @@
-
-phone_number = "+639322124220"
+# Get user input of phone number they want to track
+track_number = input("Please enter the phone number you want to track: +")
+phone_number = str("+"f"{track_number}")
